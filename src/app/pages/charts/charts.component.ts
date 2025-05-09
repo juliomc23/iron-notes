df38532a2639d20422c14f83dd9022791b47616e
juliomc23 <julito2302mc@gmail.com>
2025-05-09 12:01:47 +0200

    feat: add chart

--- src/app/pages/charts/charts.component.ts
+++ src/app/pages/charts/charts.component.ts
@@ -1,11 +1,79 @@
-import { Component } from '@angular/core';
-import { ChartModule } from 'primeng/chart'
+import { isPlatformBrowser } from '@angular/common';
+import { Component, inject, PLATFORM_ID } from '@angular/core';
+import { ChartModule } from 'primeng/chart';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 @Component({
-    selector: 'app-charts',
-    imports: [ChartModule],
-    templateUrl: './charts.component.html',
-    styleUrl: './charts.component.scss'
+  selector: 'app-charts',
+  imports: [ChartModule, FooterComponent],
+  templateUrl: './charts.component.html',
+  styleUrl: './charts.component.scss'
 })
 export class ChartsComponent {
+  basicData: any;
 
+  basicOptions: any;
+
+  platformId = inject(PLATFORM_ID);
+
+
+  ngOnInit() {
+    this.initChart();
+  }
+
+  initChart() {
+    if (isPlatformBrowser(this.platformId)) {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--p-text-color');
+      const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
+      const surfaceBorder = documentStyle.getPropertyValue('--p-content-border-color');
+
+      this.basicData = {
+        labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+        datasets: [
+          {
+            label: 'Sales',
+            data: [540, 325, 702, 620],
+            backgroundColor: [
+              'rgba(249, 115, 22, 0.2)',
+              'rgba(6, 182, 212, 0.2)',
+              'rgb(107, 114, 128, 0.2)',
+              'rgba(139, 92, 246, 0.2)',
+            ],
+            borderColor: ['rgb(249, 115, 22)', 'rgb(6, 182, 212)', 'rgb(107, 114, 128)', 'rgb(139, 92, 246)'],
+            borderWidth: 1,
+          },
+        ],
+      };
+
+      this.basicOptions = {
+        responsive: true,
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor,
+            },
+          },
+        },
+        scales: {
+          x: {  // El eje X será ahora el eje vertical (en un gráfico de barras horizontal)
+            beginAtZero: true,
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+          y: {  // El eje Y será ahora el eje horizontal
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          }
+        }
+      }
+    }
+  }
 }
